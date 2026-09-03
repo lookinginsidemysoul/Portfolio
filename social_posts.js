d3c0b4dac7f2bec53ebf921956181455a9fa2668
lookinginsidemysoul <lookinginsidemysoul@gmail.com>
2026-09-04 01:02:07 +0530

    Add public-safe social feed configuration

---
+++ social_posts.js
@@ -0,0 +1,6 @@
+/* Public-safe curated social-post feed. Never add API tokens, cookies, private URLs, or non-public content. */
+window.SOCIAL_POSTS = [];
+window.SOCIAL_PROFILES = {
+  linkedin: 'https://www.linkedin.com/in/pallab-mukherjee',
+  facebook: 'https://www.facebook.com/share/1EfdKgJwuk/?mibextid=wwXIfr'
+};
